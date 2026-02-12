5d0cc3cba964c0d34e745cb3784bc447e6a2a7f9
Your Name <your-email@example.com>
2026-02-12 11:34:16 +0530

    fine

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,29 +11,38 @@ const Header = () => {
 
 
   return (
-    <div className='grid h-3  grid-flow-col p-5 pt-3 m-2'>
-      <div className='flex justify-around col-span-1'>
-        <img onClick={()=>toggleMenuHandler()}
-        className='cursor-pointer h-7' src='https://icons.veryicon.com/png/o/miscellaneous/linear-icon-45/hamburger-menu-4.png'/>
-                </div>  
-                <div className='h-3 pt-3 object-fit: contain '>
-        <a href='/'>
-        <img className='h-14' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnEXQdAa-3ljX26kSoD1zDIGZnVP6Wyy4wJg&s'/>
-        </a>
+  <div className="grid h-3 grid-flow-col p-5 pt-3 m-2">
+    <div className="flex justify-around col-span-1">
+      <img
+        onClick={() => toggleMenuHandler()}
+        className="cursor-pointer h-7"
+        src="https://icons.veryicon.com/png/o/miscellaneous/linear-icon-45/hamburger-menu-4.png"
+        alt="Menu icon"
+      />
     </div>
-    <div className='col-span-10'>
-        <input type='text' />
-        <button>Search</button>
-        </div>
-   <div className='col-span-1 p-5 m-2'>
-     <img className='h-10' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRV-Gh6uC11b9BUzfJ1OAuC3MgwwQdOLZL7PA&s"/>
 
+    <div className="h-3 pt-3 object-contain">
+      <a href="/">
+        <img
+          className="h-14"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnEXQdAa-3ljX26kSoD1zDIGZnVP6Wyy4wJg&s"
+          alt="Website logo"
+        />
+      </a>
     </div>
+
+    <div className="col-span-10">
+      <input type="text" />
+      <button>Search</button>
     </div>
-        
-        
-       
-  )
+
+    <div className="col-span-1 p-5 m-2">
+      <img
+        className="h-10"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRV-Gh6uC11b9BUzfJ1OAuC3MgwwQdOLZL7PA&s"
+        alt="User profile"
+      />
+    </div>
+  </div>
+);
 }
-
-export default Header
